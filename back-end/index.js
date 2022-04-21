92fc0242d42b19e7bac83b729ae965fa1bbb245f
Masih-Saldanha <masih.saldanha@gmail.com>
2022-04-21 13:40:09 -0300

    Variaveis globais para armazenamento de dados criadas

--- back-end/index.js
+++ back-end/index.js
@@ -4,12 +4,32 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Requisição Get recebida");
+// FORMATO USUÁRIO:
+let user = {
+    username: '',
+    avatar: ""
+}
+
+// FORMATO TWEET:
+let tweet = {
+    username: "",
+    avatar: "",
+    tweet: "",
+}
+
+// REQUISIÇÃO DE LOGIN
+app.post("/sign-up", (req, res) => {
+    res.send("Requisição login recebida");
 });
 
-app.post("/", (req, res) => {
-    res.send("Requisição Post recebida");
+// REQUISIÇÃO DE CARREGAMENTO DE TWEETS
+app.get("/tweets", (req, res) => {
+    res.send("Requisição de carregamentos de tweets recebida");
+});
+
+// REQUISIÇÃO DE ENVIO DE TWEET
+app.post("/tweets", (req, res) => {
+    res.send("Requisição de envio de tweet recebida");
 });
 
 app.listen(5000, console.log("Server ligado na porta 5000"))
